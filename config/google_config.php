a6535408e14f10b9cabc820072714d3eb03de6cc
Bernardo Martínez <bernardo.martinez@cabify.com>
2020-01-04 12:10:58 +0100

    set deafault

--- config/google_config.php
+++ config/google_config.php
@@ -2,6 +2,6 @@
 
 return [
 
-    'google_recaptcha_key' => env('GOOGLE_RECAPTCHA_KEY', '')
+    'google_recaptcha_key' => env('GOOGLE_RECAPTCHA_KEY', '6LdLE8wUAAAAAEepO6CsC2zJvWUsp1Hn2Zx96WYW')
 
 ];
