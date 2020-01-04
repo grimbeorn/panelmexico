dd9003555a073c8746e5d59ae03e0b90128121a4
Bernardo Martínez <bernardo.martinez@cabify.com>
2020-01-04 20:56:57 +0100

    hardcode user address and name

--- config/mail.php
+++ config/mail.php
@@ -5,11 +5,11 @@ return [
     'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
     'port' => env('MAIL_PORT', 587),
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'contacto@panelmexico.com'),
-        'name' => env('MAIL_FROM_NAME', 'Panelmexico'),
+        'address' => 'contacto@panelmexico.com',
+        'name' => 'Panelmexico',
     ],
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-    'username' => env('MAIL_USERNAME', 'contacto@panelmexico.com'),
+    'username' => 'contacto@panelmexico.com',
     'password' => env('MAIL_PASSWORD', ''),
     'sendmail' => '/usr/sbin/sendmail -bs',
     'markdown' => [
