14e5dd2858ca7db88849dc9e78fa172d94de8ad0
Bernardo Martínez <bernardo.martinez@cabify.com>
2020-01-04 21:01:03 +0100

    update port

--- config/mail.php
+++ config/mail.php
@@ -3,7 +3,7 @@
 return [
     'driver' => env('MAIL_DRIVER', 'smtp'),
     'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 2525),
     'from' => [
         'address' => 'contacto@panelmexico.com',
         'name' => 'Panelmexico',
